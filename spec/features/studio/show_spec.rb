78878d9251ac7999e2f64393891ff113a2672f2d
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-06-01 09:43:11 -0600

    Create test for movies titles display on studio show page

--- spec/features/studio/show_spec.rb
+++ spec/features/studio/show_spec.rb
@@ -10,5 +10,16 @@ RSpec.describe 'Studio Show Page' do
       expect(page).to have_content(@studio_1.name)
       expect(page).to have_content(@studio_1.location)
     end
+
+    it 'shows the studios name and location' do
+      @paramount = Studio.create!(name: 'Paramount', location: 'Los Angeles')
+      @love_actually = @paramount.movies.create!(title: 'Love Actually', creation_year: 1994, genre: 'romance')
+      @terminator = @paramount.movies.create!(title: 'Terminator', creation_year: 1986, genre: 'action')
+
+      visit "/studios/#{@paramount.id}"
+
+      expect(page).to have_content(@love_actually.title)
+      expect(page).to have_content(@terminator.title)
+    end
   end
 end
