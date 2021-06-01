2ec02dda8ec587d2ad7722eed1f371966f22dbed
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-06-01 10:38:27 -0600

    Implement story 2

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -2,5 +2,6 @@ class StudiosController < ApplicationController
   def show
     @studio = Studio.find(params[:id])
     @studio_movies = @studio.movies
+    @movie_actors = Studio.actors_list
   end
 end
